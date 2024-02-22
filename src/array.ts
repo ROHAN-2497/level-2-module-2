a5dadc9611f3e2f75b4461f33d4378e9d83b6b2f
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-23 02:06:20 +0600

    array --> tuple type Array

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+const names = ["rohan", "raihan", 36, true]
+
+// tuple
+// 1 -->
+const boyfriends: [string, boolean] = ['abul', true]
+
+// 2 -->
+const user: [number, string, Boolean] = [121, "abul", false]
