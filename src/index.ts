46d1327ba5055bc380979f470ad0eb569952c734
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-23 02:00:39 +0600

    prenitive type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 
-const course: string = "next level  dev"
-console.log(course);
+let course: string = "next level  dev"
+course="next level"
+let rollNumber :number = 123
+rollNumber=32323
+let isAdmin : boolean = true
+isAdmin=false
+
+// premative types
+// string
+// number
+// boolean
+// null
+// undefined
