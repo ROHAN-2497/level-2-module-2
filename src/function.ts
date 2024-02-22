9c72a32ced8cffb92a34613794090642d678f9a6
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-23 02:45:44 +0600

    up

---
+++ src/function.ts
@@ -0,0 +1,12 @@
+// NORMAL FUNCTION
+
+function add(num1: number, num2: number) {
+    return num1 + num2
+}
+add(2, 2)
+
+const addArrow = (num3: number, num4: number) : number => num3 + num4
+
+const arr = [1, 23, 43]
+
+const newArr = arr.map((elem: number) => elem * elem);
