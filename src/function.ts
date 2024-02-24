e07c853d81b6d6f59955b3a700216fb3440d251c
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-25 03:01:55 +0600

    up

--- src/function.ts
+++ src/function.ts
@@ -1,7 +1,15 @@
 // spread oparator
-const myFriends =  ['chandeler', 'jopye', 'richa']
-const newFriends = ['michel', 'pheobe', 'monica']
+const myFriends = ["chandeler", "jopye", "richa"];
+const newFriends = ["michel", "pheobe", "monica"];
 
-myFriends.push(...newFriends)
+myFriends.push(...newFriends);
 console.log(myFriends);
 
+// rest parameter
+
+const greetFriends = (
+  friend1: string,
+  friend2: string,
+  friend3: string
+): void => console.log(`hi ${friend1}\n hi ${friend2}\n hi${friend3}`);
+greetFriends("kashem", "hashem", "gashem");
