432822bc36c92432aa9f91f70939b059073d3a0a
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-26 13:51:07 +0600

    never type

--- src/nullable-unknown-never.ts
+++ src/nullable-unknown-never.ts
@@ -1,12 +1,15 @@
+// nullable type
 const searchName = (value: string | null) => {
   if (value === null) {
-    // console.log("there is nothing search");
+    console.log("there is nothing search");
   } else {
-    // console.log("there is search");
+    console.log("there is search");
   }
 };
 
 searchName(null);
+
+// unknown Type
 
 const getMyCarSpeed = (speed: unknown) => {
   if (typeof speed === "number") {
@@ -17,8 +20,18 @@ const getMyCarSpeed = (speed: unknown) => {
     const [value, unit] = speed.split(" "); // [10, 'kmh^-1']
     const converted = (parseFloat(value) * 1000) / 3600;
     console.log(`my speed is ${converted}`);
+  } else {
+    console.log("there is worong type");
   }
 };
 
 getMyCarSpeed(10);
 getMyCarSpeed("10 kmh^ -1");
+getMyCarSpeed(true);
+
+// never type
+function throwError(message: string) {
+  throw new Error(message);
+}
+
+throwError("bhai error khaichhi kandi dimu");
