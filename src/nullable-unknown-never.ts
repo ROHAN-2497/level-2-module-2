e24c69fdc563221db693a1cef63c66d8cf7bab4b
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-26 13:41:14 +0600

    updated

--- src/nullable-unknown-never.ts
+++ src/nullable-unknown-never.ts
@@ -1,9 +1,24 @@
 const searchName = (value: string | null) => {
   if (value === null) {
-    console.log("there is nothing search");
+    // console.log("there is nothing search");
   } else {
-    console.log("there is search");
+    // console.log("there is search");
   }
 };
 
-searchName(null)
+searchName(null);
+
+const getMyCarSpeed = (speed: unknown) => {
+  if (typeof speed === "number") {
+    const converted = (speed * 1000) / 3600;
+    console.log(`my speed is ${converted}`);
+  }
+  if (typeof speed === "string") {
+    const [value, unit] = speed.split(" "); // [10, 'kmh^-1']
+    const converted = (parseFloat(value) * 1000) / 3600;
+    console.log(`my speed is ${converted}`);
+  }
+};
+
+getMyCarSpeed(10);
+getMyCarSpeed("10 kmh^ -1");
