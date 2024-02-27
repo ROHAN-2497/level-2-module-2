19a2327d43913185c9fc44393103811a1f10aa55
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-28 03:08:07 +0600

    boolean type Promise

--- src/module-3/asynchronous.ts
+++ src/module-3/asynchronous.ts
@@ -14,3 +14,19 @@ const getPromiseData = async (): Promise<string> => {
   const data = await makePromise();
   return data;
 };
+
+const makePromiseBoolean = (): Promise<Boolean> => {
+  return new Promise<boolean>((resolve, reject) => {
+    const data: boolean = true;
+    if (data) {
+      resolve(data);
+    } else {
+      reject("data is not Fatched");
+    }
+  });
+};
+
+const getPromiseDataBoolean = async () => {
+  const data = await makePromiseBoolean();
+  return data;
+};
