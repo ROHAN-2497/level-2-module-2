841f8888c447e742b8d4a840f8791cf2467ed241
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-27 14:19:10 +0600

    interface type and typealias

---
+++ src/module-3/interface.ts
@@ -0,0 +1,47 @@
+// type User = {
+//   name: string;
+//   age: number;
+// };
+
+// interface IUser {
+//   name: string;
+//   age: number;
+// }
+
+// type extendedUser = User & {
+//   role: string;
+// };
+
+// interface IExtended extends IUser {
+//   role: string;
+// }
+
+// const userss: IExtended = {
+//   name: "rohan",
+//   age: 20,
+//   role: "unknown",
+// };
+
+// const userWithTypeAlias: User = {
+//   name: "rohan",
+//   age: 22,
+// };
+
+// const userWithInterface: IUser = {
+//   name: "rohan",
+//   age: 22,
+// };
+
+// type addNumbersType = (num1: number, num2: number) => number;
+// interface IAddNumber {
+//   (num1: number, num2: number): number;
+// }
+
+// const addNumbers: IAddNumber = (num1, num2) => num1 + num2;
+
+// type RollNumbersTypes = number[];
+// interface IRollNumbers {
+//   [index: number]: number;
+// }
+
+// const rollnumbers: IRollNumbers = [1, 2, 3];
