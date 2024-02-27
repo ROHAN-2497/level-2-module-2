0388d0035b89fde346b806ad85b554250e2af321
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-28 03:14:08 +0600

    promise Object type

--- src/module-3/asynchronous.ts
+++ src/module-3/asynchronous.ts
@@ -15,6 +15,26 @@ const getPromiseData = async (): Promise<string> => {
   return data;
 };
 
+interface DataInterface {
+  data: string;
+}
+
+const makePromiseObject = (): Promise<DataInterface> => {
+  return new Promise<DataInterface>((resolve, reject) => {
+    const data: DataInterface = { data: "Data is fatched" };
+    if (data) {
+      resolve(data);
+    } else {
+      reject("Data is not Fatched");
+    }
+  });
+};
+
+const getPromiseDataObject = async (): Promise<object> => {
+  const data = await makePromiseObject();
+  return data;
+};
+
 const makePromiseBoolean = (): Promise<Boolean> => {
   return new Promise<boolean>((resolve, reject) => {
     const data: boolean = true;
