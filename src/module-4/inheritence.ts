ff0b6890ce93700499adfd495d7dfc60095d6254
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-29 00:40:18 +0600

    new class contructor and method

--- src/module-4/inheritence.ts
+++ src/module-4/inheritence.ts
@@ -12,3 +12,25 @@ class Student {
     return `this ${this.name} will sleep for ${hours}`;
   }
 }
+
+class Teacher {
+  name: string;
+  age: number;
+  address: string;
+  designation: string;
+
+  constructor(name: string, age: number, address: string, designation: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+    this.designation = designation;
+  }
+
+  makeSleep(hours: number): string {
+    return `this ${this.name} will sleep for ${hours}`;
+  }
+
+  ClassName(numOfClass: number): string {
+    return `this ${this.name} will take ${numOfClass}`;
+  }
+}
