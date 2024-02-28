76bdc33db62a4646ee32b37760d9b773ef482c6f
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-29 02:58:30 +0600

    up

--- src/module-4/access-modifiar.ts
+++ src/module-4/access-modifiar.ts
@@ -1,13 +1,21 @@
 class MyBankAccout {
   id: number;
   name: string;
-  private balance: number;
+  balance: number;
   constructor(id: number, name: string, balance: number) {
     this.id = id;
     this.name = name;
     this.balance = balance;
   }
+  getBalance() {
+    console.log(`my current balance is ${this.balance}`);
+  }
+  addDeposite(money: number) {
+    this.balance = this.balance + money;
+  }
 }
 
 const Account = new MyBankAccout(444, "parsian", 4000);
 console.log(Account);
+Account.addDeposite(20);
+Account.getBalance();
