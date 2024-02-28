f556bd1bcd7d30aa2ae8da76011f9736930a8901
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-28 22:53:20 +0600

    up

--- src/module-3/mapped-type.ts
+++ src/module-3/mapped-type.ts
@@ -1,3 +1,26 @@
 const arrayOfNumbers = [1, 2, 3, 4, 5];
 const arrayOfStrig = arrayOfNumbers.map((number) => number.toString());
 console.log(arrayOfStrig);
+type AreaNumber = {
+  height: number;
+  width: number;
+};
+type AreaString = {
+  height: string;
+  width: string;
+};
+
+type ReadOnly = {
+  readonly height: number;
+  readonly width: number;
+};
+
+const reactAngularArea: ReadOnly = {
+  height: 10,
+  width: 12,
+};
+
+reactAngularArea;
+
+type A1 = AreaNumber["height"]; // LOOK UP TYPES
+type b1 = keyof AreaNumber;
