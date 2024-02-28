51957d06366b325ef86de6fcabac60c19cbad642
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-29 01:35:49 +0600

    up

--- src/module-4/inheritence.ts
+++ src/module-4/inheritence.ts
@@ -1,39 +1,39 @@
-class Parent {
-  name: string;
-  age: number;
-  address: string;
+// class Parent {
+//   name: string;
+//   age: number;
+//   address: string;
 
-  constructor(name: string, age: number, address: string) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-  }
-  makSleep(hours: number): string {
-    return `this ${this.name} will sleep for ${hours}`;
-  }
-}
+//   constructor(name: string, age: number, address: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+//   }
+//   makSleep(hours: number): string {
+//     return `this ${this.name} will sleep for ${hours}`;
+//   }
+// }
 
-// class 1
+// // class 1
 
-class Student extends Parent {
-  constructor(name: string, age: number, address: string) {
-    super(name, age, address);
-  }
-}
-const student1 = new Student("rohan", 22, "ishurdi");
+// class Student extends Parent {
+//   constructor(name: string, age: number, address: string) {
+//     super(name, age, address);
+//   }
+// }
+// const student1 = new Student("rohan", 22, "ishurdi");
 
-// class 2
+// // class 2
 
-class Teacher extends Parent {
-  designation: string;
+// class Teacher extends Parent {
+//   designation: string;
 
-  constructor(name: string, age: number, address: string, designation: string) {
-    super(name, age, address);
-    this.designation = designation;
-  }
+//   constructor(name: string, age: number, address: string, designation: string) {
+//     super(name, age, address);
+//     this.designation = designation;
+//   }
 
-  ClassName(numOfClass: number): string {
-    return `this ${this.name} will take ${numOfClass}`;
-  }
-}
-const teacher1 = new Teacher("Mr. Y", 54, "ishurdi", "professor");
+//   ClassName(numOfClass: number): string {
+//     return `this ${this.name} will take ${numOfClass}`;
+//   }
+// }
+// const teacher1 = new Teacher("Mr. Y", 54, "ishurdi", "professor");
--- src/module-4/type-gurd.ts
+++ src/module-4/type-gurd.ts
@@ -1,8 +1,8 @@
 // keyOf gurd
-function add(
-  param1: string | number,
-  param2: string | number
-): string | number {
+
+type AlphaNumeric = string | number;
+
+function add(param1: AlphaNumeric, param2: AlphaNumeric): AlphaNumeric {
   if (typeof param1 === "number" && typeof param2 === "number") {
     return param1 + param2;
   } else {
@@ -12,3 +12,26 @@ function add(
 
 add("1", "2");
 add(2, 3);
+
+// type of IN gurd
+
+type NormalUserType = {
+  name: string;
+};
+type AdminUserType = {
+  name: string;
+  role: "admin";
+};
+function getUser(user: NormalUserType | AdminUserType): string {
+  if ("role" in user) {
+    return `I am an Admin User my Roll is ${user.role}`;
+  } else {
+    return `i am a Normal User ${user.name}`;
+  }
+}
+
+const user1: NormalUserType = { name: "Mr. X" };
+const user2: AdminUserType = { name: "Mr. Y", role: "admin" };
+
+console.log(getUser(user1));
+console.log(getUser(user2));
