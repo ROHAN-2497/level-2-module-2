21aa4019aea463b4c4155c0b31013b1ddd60575a
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-29 00:31:37 +0600

    inheritance

---
+++ src/module-4/inheritence.ts
@@ -0,0 +1,14 @@
+class Student {
+  name: string;
+  age: number;
+  address: string;
+
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+  makeSleep(hours: number): string {
+    return `this ${this.name} will sleep for ${hours}`;
+  }
+}
