93ed5178cf5292a5e3ff2af53f9cfeb6161cc98d
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-28 23:50:37 +0600

    how to creat class, object, parameter properties

--- src/module-4/object.ts
+++ src/module-4/object.ts
@@ -1,16 +1,23 @@
 class Animal {
-  name: string;
-  species: string;
-  sound: string;
+  // TYPE DECLARE
+  //   public name: string;
+  //   public species: string;
+  // public sound: string;
 
-  constructor(name: string, species: string, sound: string) {
-    (this.name = name), (this.species = species), (this.sound = sound);
+  //   MAKE CONNSTRUCTOR
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {
+    // (this.name = name), (this.species = species), (this.sound = sound);
   }
-  makeSound() {
+  //   MAKE METHOD
+  public makeSound() {
     console.log(`the ${this.name} says ${this.sound}`); // 'GARMAN SHEPHARD SAYS GHEW GHEW'
   }
 }
-
+// INSTANCE MAKE
 const dog = new Animal("GARMAN SHEPHARD", "dog", "ghew ghew");
 const cat = new Animal("parsian", "cat", "mew mew");
 dog.makeSound();
