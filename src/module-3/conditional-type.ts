808cfe46cd420e65936c992187b8442da0c9d90b
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-28 13:02:36 +0600

    conditional type with nested

---
+++ src/module-3/conditional-type.ts
@@ -0,0 +1,22 @@
+type a = string;
+type b = a extends string ? string : null;
+type c = undefined;
+// nested CONDITIONAL type
+
+type d = c extends number ? number : a extends boolean ? boolean : never;
+
+type Sheikh = {
+  wife1: string;
+  wife2: string;
+};
+
+type A = keyof Sheikh;
+
+type cheakProperty<T, W> = W extends keyof Sheikh ? true : false;
+type CheakWife = cheakProperty<Sheikh, "wife1">;
+
+type Bandhobi = "monika" | "rechel" | "pheobe";
+
+type RemoveBandhobi<T> = T extends "rechel" ? never : T;
+
+type Result = RemoveBandhobi<Bandhobi>;
