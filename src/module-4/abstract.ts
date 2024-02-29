8605b3981b9d2776da0f75201168348ba60fa970
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-29 15:16:53 +0600

    abstract

---
+++ src/module-4/abstract.ts
@@ -0,0 +1,18 @@
+// INTERFACE
+
+// interface IVehcle {
+//   name: string;
+//   model: string;
+// }
+// const vehcle: IVehcle = {
+//   name: "car",
+//   model: "200rk",
+// };
+
+interface IVehicle {
+  startEngine(): void;
+  stopEngine(): void;
+  moveEngine(): void;
+}
+
+// ABSTRACT CLASS
