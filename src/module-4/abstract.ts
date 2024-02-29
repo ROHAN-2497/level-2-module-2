cd824fa3817db2f63e5079a54583f572c08532cf
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-29 20:52:19 +0600

    abstract class

--- src/module-4/abstract.ts
+++ src/module-4/abstract.ts
@@ -16,6 +16,12 @@ interface IVehicle {
 }
 
 class Vehicle implements IVehicle {
+  constructor(
+    public name: string,
+    public brand: string,
+    public model: number
+  ) {}
+
   startEngine(): void {
     console.log("this is start engine");
   }
@@ -29,5 +35,26 @@ class Vehicle implements IVehicle {
     console.log("I am testing for purpose");
   }
 }
+const vehicle1 = new Vehicle("Range Rover", "black", 2024);
 
 // ABSTRACT CLASS
+
+abstract class NewVehicle {
+  abstract start(): void;
+  abstract stop(): void;
+  move() {
+    console.log("this is move part");
+  }
+  test() {
+    console.log("this is test");
+  }
+}
+
+class Vehicles extends NewVehicle {
+  start(): void {
+    console.log("this is start");
+  }
+  stop(): void {
+    console.log("this is start");
+  }
+}
