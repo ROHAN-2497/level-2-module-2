b0ec021579050bc14702afdce54176e15966c509
ROHAN-2497 <rohanislam3669@gmail.com>
2024-03-02 03:35:07 +0600

    PICK and OMIT

--- src/module_5-5/utility.ts
+++ src/module_5-5/utility.ts
@@ -8,4 +8,16 @@ interface Person {
   contactNo: string;
 }
 
-type Contact = Pick<Person, "name">;
+type Contact = Pick<Person, "contactNo" | "email">;
+
+{
+  /* OMIT */
+}
+
+type Person2 = {
+  name: string;
+  age: 32;
+  address: string;
+};
+
+type User = Omit<Person2, "name" | "address">;
